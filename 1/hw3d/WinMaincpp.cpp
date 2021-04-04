90192152c80d6a24e2b054bf3c64b45dd1718869
kimtaehyung <taehyeongkim77@gmail.com>
2021-04-04 12:11:52 +0900

    1

---
+++ 1/hw3d/WinMaincpp.cpp
@@ -0,0 +1,20 @@
+/*
+// Error
+int main()
+{
+	return 0;
+}
+*/
+
+#include <Windows.h>
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	while (true);
+
+	return 0;
+}
